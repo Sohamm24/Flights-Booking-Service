1d7e937887f8eb539aec521f9371bb8c07deb193
unknown <sohamnarvankar24@gmail.com>
2025-04-25 15:22:51 +0530

    Seats model completed

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository=require('./crud-repository')
-const { Flight } = require('../models')
+const { Flight, Airplane , Airport , City} = require('../models')
+const {Sequelize} =require('sequelize')
 
 class FlightRepository extends CrudRepository {
     constructor(){
@@ -9,7 +10,34 @@ class FlightRepository extends CrudRepository {
     async getAllFlights(filter,sort){
         const response=await Flight.findAll({
             where: filter,
-            order:sort
+            order:sort,
+            include:[
+                {
+                    model : Airplane,
+                    required : true,
+                    as : 'airplaneDetail'
+                },
+                {
+                    model: Airport,
+                    required : true,
+                    as : 'departureAirport',
+                    on : Sequelize.where(Sequelize.col("Flight.departureAirportId"),"=",Sequelize.col("departureAirport.code")),
+                    include : {
+                        model :  City,
+                        required : true
+                    }
+                },
+                {
+                    model: Airport,
+                    required : true,
+                    as : 'arrivalAirport',
+                    on : Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),"=",Sequelize.col("arrivalAirport.code")),
+                    include : {
+                        model :  City,
+                        required : true
+                    }
+                }
+            ]
         })
      return response
     }
